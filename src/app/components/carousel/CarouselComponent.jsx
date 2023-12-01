6412d300ec7f87bfd84c81395959baee65ce06f2
FabriSartor <fabriziosartorsaita@gmail.com>
2023-12-01 02:43:57 -0300

    cambio en el estilo de los bullets

--- src/app/components/carousel/CarouselComponent.jsx
+++ src/app/components/carousel/CarouselComponent.jsx
@@ -1,7 +1,7 @@
 export default function CarouselComponent ({img}) {
     return (
         <div>
-            <img width={1600} height={420} src={img}  alt="Gordito Barasi" />
+            <img width={1600} height={420} src={img}  alt="Gordito Barasi"/>
         </div>
     )
 }
